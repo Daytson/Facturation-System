10df7a908859e4b576659fd3a2eca1b65573f0a8
Daytson Bobadilla <daytsonbobadilla@outlook.com>
2023-12-10 16:07:51 -0400

    Data and first controller OK

---
+++ Facturation System/Facturation System/Models/Producto.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Facturation_System.Models
+{
+    public partial class Producto
+    {
+        public string? Item { get; set; }
+        public string? Descripcion { get; set; }
+        public int? CantidadEnExistencia { get; set; }
+        public double? Costo { get; set; }
+        public double? PrecioDeVenta { get; set; }
+        public double? Impuesto { get; set; }
+        public int? EstatusProducto { get; set; }
+        public string? BarCode { get; set; }
+        public byte[]? Imagen { get; set; }
+        public string? Ruta { get; set; }
+    }
+}
